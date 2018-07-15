6144d0da0294918f370307e37df3d4335e8e8306
ib code <alfa_lab@sina.com>
2018-07-15 15:24:11 +0800

    remove proxy 7070-7072 port

--- start_swam_dev_noproxy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#docker network create   -d overlay ags_network
-#docker service create --name ags_proxy --publish "mode=host,target=80,published=80" --publish "mode=host,target=443,published=443" --publish "mode=host,target=7070,published=7070" --publish "mode=host,target=7070,published=7071" --publish "mode=host,target=7072,published=7072"  --mode global --network ags_network registry.cn-hangzhou.aliyuncs.com/ags/image_nginx_proxy:latest
-docker stack deploy ags --compose-file=./docker-compose-dev.yml
-
