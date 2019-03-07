4376e83b1aceda4fce74b74c092e1f6612c863fa
ib code <alfa_lab@sina.com>
2019-03-07 09:14:18 +0800

    add no proxy

--- install_docker.sh
+++ install_docker.sh
@@ -5,5 +5,7 @@ sudo yum remove docker docker-common docker-selinux docker-engine -y
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 sudo yum makecache fast
-sudo yum install docker-ce -y
+#sudo yum install docker-ce -y
+sudo  yum  install docker-ce-17.12.0.ce-1.el7.centos.x86_64
 sudo systemctl start docker
+sudo systemctl enable docker
--- start_dev.sh
+++ start_dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+#docker network create   -d overlay ags_network
 docker stack deploy ags  --compose-file=docker-compose-dev.yml
 # docker service create    --name ags_proxy --publish "mode=host,target=80,published=80" --publish "mode=host,target=443,published=443"    --network  ags_network  registry.cn-hangzhou.aliyuncs.com/ags/image_openresty_proxy:latest
 
